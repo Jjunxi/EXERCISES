381bc6e4e0117dbd67b75ec49ad6afef3b123fc7
jjunxi <jjunxi417@gmail.com>
2017-02-19 23:37:48 +0800

    question description

--- reverse_sentence.py
+++ reverse_sentence.py
@@ -2,7 +2,29 @@
 # -*- coding: utf-8 -*-
 
 """
-'this is a book'
+' this is   a book  !  '
 
-'book a is this'
+'  !  book a   is this '
 """
+
+orig = ' this is   a book  !  '
+print(orig)
+ret = ''
+aux = []
+idx = 0
+need_append = False
+
+for ch in orig:
+    if not ch.isalpha():
+        aux.append(ch)
+        need_append = True
+    else:
+        if need_append:
+            aux.append('')
+            need_append = False
+        # 此处不用idx控制，而是直接用-1对最后一个elem操作
+        aux[-1] += ch
+# reverse acts itself and returns None
+aux.reverse()
+ret = ''.join(aux)
+print(ret)
