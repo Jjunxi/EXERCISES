8599e5db32ada4da0026e558b8660314189ea2d6
LIU-YQ <lyqcareer@gmail.com>
2017-02-20 11:19:38 +0800

    restructure with main and exception

--- reverse_sentence.py
+++ reverse_sentence.py
@@ -5,26 +5,56 @@
 ' this is   a book  !  '
 
 '  !  book a   is this '
+
+不是简单的分词，空格需要保留。
 """
 
-orig = ' this is   a book  !  '
-print(orig)
-ret = ''
-aux = []
-idx = 0
-need_append = False
+import sys
+import os
 
-for ch in orig:
-    if not ch.isalpha():
-        aux.append(ch)
-        need_append = True
-    else:
-        if need_append:
-            aux.append('')
-            need_append = False
-        # 此处不用idx控制，而是直接用-1对last elem操作
-        aux[-1] += ch
-# reverse acts itself and returns None
-aux.reverse()
-ret = ''.join(aux)
-print(ret)
+
+class Usage(Exception):
+    def __init__(self, expt):
+        self.exception = expt
+        self.info = '***Exception*** \n' + str(expt)
+
+
+def reverse_sentence(orig):
+    aux = []
+    need_append = False
+
+    for ch in orig:
+        if not ch.isalpha():
+            aux.append(ch)
+            need_append = True
+        else:
+            if need_append:
+                aux.append('')
+                need_append = False
+            # 此处不用idx控制，而是直接用-1对last elem操作
+            aux[-1] += ch
+    # reverse acts itself and returns None
+    aux.reverse()
+    ret = ''.join(aux)
+    return ret
+
+
+def test():
+    orig = ' this is   a book  !  '
+    print(orig)
+    print(reverse_sentence(orig))
+
+
+def main():
+    try:
+        try:
+            test()
+        except Exception as e:
+            raise Usage(e)
+    except Usage as usg:
+        print(usg.info)
+        return -1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
