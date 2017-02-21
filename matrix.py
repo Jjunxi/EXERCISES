605569aab76d66f361d4771503e07c572f8d8223
LIU-YQ <lyqcareer@gmail.com>
2017-02-21 15:01:56 +0800

    transpose 二维数组的列表解析

--- matrix.py
+++ matrix.py
@@ -4,32 +4,58 @@
 """
 matrix exercises
 多维列表
-B[i][j] = A[0][0] + ... + A[i][j]
 """
 
 from common import *
 import random
-
-ROW_NUM = 3
-COL_NUM = 2
+import numpy as np
 
 
-def my_sum(matrix):
-    # 构造多维列表时，一行一行构造
+def generate_matrix(row_num, col_num, is_random=False):
     rst = []
-    for i in range(ROW_NUM):
-        line = []
-        for j in range(COL_NUM):
-            l = [matrix[x][y] for x in range(i + 1) for y in range(j + 1)]
-            line.append(sum(l))
-        rst.append(line)
+    if is_random:
+        rst = [[random.randint(1, row_num * col_num + 1) for i in range(col_num)] for i in range(row_num)]
+    else:
+        rst = np.arange(1, row_num * col_num + 1).reshape(row_num, col_num)
+    print(rst)
     return rst
 
 
+# B[i][j] = A[0][0] + ... + A[i][j]
+def my_sum(matrix):
+    row_num = len(matrix)
+    col_num = len(matrix[0])
+
+    # 构造多维列表时，一行一行构造
+    rst = []
+    for i in range(row_num):
+        line = []
+        for j in range(col_num):
+            l = [matrix[x][y] for x in range(i + 1) for y in range(j + 1)]
+            line.append(sum(l))
+        rst.append(line)
+    print(rst)
+    return rst
+
+
+# transpose
+def transpose(matrix):
+    col_num = len(matrix[0])
+    rst = [[x for x in matrix[:, i]] for i in range(col_num)]
+    print(rst)
+    return rst
+
+
+def spiral_order(matrix):
+    pass
+
+
 def demo():
-    matrix = [[random.randint(1, 9) for i in range(COL_NUM)] for i in range(ROW_NUM)]
-    print(matrix)
-    print(my_sum(matrix))
+    org = generate_matrix(2, 3, True)
+    my_sum(org)
+
+    org = generate_matrix(3, 5)
+    transpose(org)
 
 
 def main():
